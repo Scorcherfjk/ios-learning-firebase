e58f2a374a204d54dcab710f6ab7edf635191c20
scorcherfjk <franjavikale@gmail.com>
2021-01-01 00:18:20 -0500

    Initial Commit

---
+++ Learning Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Learning Firebase
+//
+//  Created by Francisco De Freitas on 1/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
